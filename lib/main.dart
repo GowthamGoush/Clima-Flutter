88c8397c9f6db40eda9a17868550a015bd001201
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-17 12:14:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:clima_flutter_app/screens/loading_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(),
-    ),
+    theme: ThemeData.dark(),
+    home: LoadingScreen(),
   ));
 }
